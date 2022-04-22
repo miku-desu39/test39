a59333c6e5863eb51d8a437a8f8e56f3db8797a6
allangao <allangao@tencent.com>
2022-04-22 18:01:23 +0800

    文件上传跟新

--- second-vue/src/component/CompanyCenter.vue
+++ second-vue/src/component/CompanyCenter.vue
@@ -45,6 +45,26 @@
             </el-form-item>
 
             <el-form-item>
+              <el-upload
+                class="upload-demo"
+                drag
+                :action="'/api/company/uploadIcon/?companyId='+companyId"
+                :before-upload="beforeAvatarUpload"
+                multiple
+                :limit="1"
+                with-credentials:true
+              >
+                <i class="el-icon-upload"></i>
+                <div class="el-upload__text">
+                  将文件拖到此处，或<em>点击上传</em>
+                </div>
+                <div class="el-upload__tip" slot="tip">
+                  只能上传jpg文件，且不超过2mb
+                </div>
+              </el-upload>
+            </el-form-item>
+
+            <el-form-item>
               <el-button
                 type="primary"
                 @click="saveCompanyDetail('company')"
@@ -514,6 +534,23 @@ export default {
   },
 
   methods: {
+
+
+     beforeAvatarUpload(file) {
+        const isJPG = file.type === 'image/jpeg';
+        const isLt2M = file.size / 1024 / 1024 < 2;
+
+        if (!isJPG) {
+          this.$message.error('上传头像图片只能是 JPG 格式!');
+        }
+        if (!isLt2M) {
+          this.$message.error('上传头像图片大小不能超过 2MB!');
+        }
+        return isJPG && isLt2M;
+      },
+
+
+
     getCompanyDetail() {
       this.$axios
         .get("/api/company/getCompanyDetail?companyId=" + this.companyId)
